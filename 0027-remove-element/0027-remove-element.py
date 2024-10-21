c145a0ffd21c1229a7efd8fb06e58d91548262a0
StepanPotiienko <138608284+StepanPotiienko@users.noreply.github.com>
2024-10-21 09:42:58 +0300

    Time: 4 ms (97.07%), Space: 16.8 MB (6.77%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        self.erase_val_from_array(val, nums)
+        while val in nums: 
+            self.erase_val_from_array(val, nums)
+        return len(nums)
+
+    def erase_val_from_array(self, val, array):
+        for i in array:
+            print(i)
+            if i == val:
+                array.remove(i)
+    
