f6c47a7b14bcdc4f7408863792f0feec431ea2dd
StepanPotiienko <138608284+StepanPotiienko@users.noreply.github.com>
2024-10-21 09:40:43 +0300

    Time: 0 ms (100%), Space: 4 MB (5.13%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
@@ -0,0 +1,18 @@
+import (
+    "strings"
+    "fmt"
+)
+
+func strStr(haystack string, needle string) int {
+    if strings.Contains(haystack, needle) {
+        for i := 0; i < len(haystack); i++ {
+            if len(needle) + i > len(haystack) {
+                return -1
+            } else if haystack[i:len(needle)+i] == needle {
+                return i
+            }
+        }
+    }
+
+    return -1
+}
